4d44edb98accea1ffec4926e5cea2a3733192679
BotMaster3000 <jdgebauer@gmx.de>
2017-07-01 22:34:32 +0200

    Added function functionality
    Sleep, Train, Break, and Shoot functions now work properly

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,18 +20,38 @@ namespace ArcherySimulator
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        public ObservableCollection<string> EventLog = new ObservableCollection<string>();
+        public event PropertyChangedEventHandler PropertyChanged;
+
         Random rand = new Random();
-        string currentStamina;
-        public string CurrentStamina
+        private ObservableCollection<string> eventLog;
+        public ObservableCollection<string> EventLog
         {
             get
             {
-                if(currentStamina == null)
+                if(eventLog == null)
                 {
-                    currentStamina = "N/A";
+                    eventLog = new ObservableCollection<string>();
+                }
+                return eventLog;
+            }
+            set
+            {
+                if(eventLog != value)
+                {
+                    eventLog = value;                    
+                }
+            }
+        }
+        private int currentStamina = -1;
+        public int CurrentStamina
+        {
+            get
+            {
+                if(currentStamina == -1)
+                {
+                    currentStamina = 100;
                 }
                 return currentStamina;
             }
@@ -40,18 +60,15 @@ namespace ArcherySimulator
                 if(currentStamina != value)
                 {
                     currentStamina = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("CurrentStamina"));
                 }
             }
         }
-        string currentExperience;
-        public string CurrentExperience
+        private int currentExperience;
+        public int CurrentExperience
         {
             get
             {
-                if(currentExperience == null)
-                {
-                    currentExperience = "0";
-                }
                 return currentExperience;
             }
             set
@@ -59,17 +76,18 @@ namespace ArcherySimulator
                 if(currentExperience != value)
                 {
                     currentExperience = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("CurrentExperience"));
                 }
             }
         }
-        string currentLevel;
-        public string CurrentLevel
+        private int currentLevel;
+        public int CurrentLevel
         {
             get
             {
-                if(currentLevel == null)
+                if(currentLevel == 0)
                 {
-                    currentLevel = "1";
+                    currentLevel = 1;
                 }
                 return currentLevel;
             }
@@ -78,6 +96,7 @@ namespace ArcherySimulator
                 if(currentLevel != value)
                 {
                     currentLevel = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("CurrentLevel"));
                 }
             }
         }
@@ -94,31 +113,53 @@ namespace ArcherySimulator
             }
             EventLog.Add(entry);
         }
+        private bool HasEnoughStamina(int requiredStamina)
+        {
+            if((CurrentStamina - requiredStamina) >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
         public void Train(object sender, RoutedEventArgs e)
         {
-
+            for(int i = 0; i < 10; ++i)
+            {
+                Shoot(new object(), new RoutedEventArgs());
+            }
         }
         public void Sleep(object sender, RoutedEventArgs e)
         {
-            CurrentStamina = "100";
+            CurrentStamina = 100;
             AddToLog("You went to sleep");
         }
         public void Break(object sender, RoutedEventArgs e)
         {
-            int stamina = Convert.ToInt32(CurrentStamina);
+            int stamina = CurrentStamina;
             stamina += 50;
             if(stamina > 100)
             {
                 stamina = 100;
             }
-            CurrentStamina = stamina.ToString();
+            CurrentStamina = stamina;
         }
         public void Shoot(object sender, RoutedEventArgs e)
         {
-            int result = (int)(1 / (rand.Next(1, 11) + Math.Round(0.1 * Convert.ToInt32(CurrentLevel))));
-            AddToLog("You hit a " + result.ToString());
-            CurrentExperience += result;
-            AddToLog("You received " + result.ToString() + " experience");
+            int requiredStamina = 10;
+            if (!HasEnoughStamina(requiredStamina))
+            {
+                AddToLog("You dont have enough Stamina");
+                return;
+            }
+            CurrentStamina -= requiredStamina;
+
+            int result = (int) (rand.Next(1, 11) + Math.Round(0.1 * CurrentLevel));
+            AddToLog("You hit a " + result);
+            CurrentExperience += result;            
+            AddToLog("You received " + result + " experience");
         }
     }
 }
